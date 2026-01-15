e13bdd99542bd1ae42500f836bedbc546d80f318
Neelesh Singh <neeleshchoursiya0786@gmail.com>
2026-01-15 11:02:09 +0530

    pasted the frontend url in  CORS

--- cartoon-network-go/backend/src/router/router.go
+++ cartoon-network-go/backend/src/router/router.go
@@ -22,7 +22,7 @@ func SetupRouter() *gin.Engine {
 	// CORS CONFIG
 	// =========================================
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://127.0.0.1:5500", "http://localhost:5500"},
+		AllowOrigins:     []string{" https://cartoon-network-go-frontend.onrender.com "},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
